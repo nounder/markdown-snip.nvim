dbadf33d47fa3aab6dba85f595ab4b5b4433c37a
Rafael Gutkowski <nounderline@libred.org>
2025-07-02 13:50:14 -0400

    sample lazy config

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ Position cursor in a markdown code block and press `gd` to edit it in a dedicate
 
 Type `[[path]]`, `[text](path)`, or `@path` to trigger file completion from your project directory using `fd`, `rg`, or `find` (respects `.gitignore`).
 
+---
+
+See `examples/lazy-config.lua` for sample LAzy configuration.
+
---
+++ examples/lazy-config.lua
@@ -0,0 +1,25 @@
+-- Lazy plugin configuration for markdown-snip.nvim
+-- Place this ~/.config/nvim/lua/plugins/markdown-snip.lua
+
+return {
+	"nounder/markdown-snip.nvim",
+	ft = "markdown",
+	keys = {
+		{
+			"gd",
+			function()
+				require("markdown-snip").goto_file()
+			end,
+			ft = "markdown",
+			desc = "Go to file/code block under cursor",
+		},
+		{
+			"<leader>mf",
+			function()
+				require("markdown-snip").insert_file_reference()
+			end,
+			ft = "markdown",
+			desc = "Insert file reference using picker",
+		},
+	},
+}
